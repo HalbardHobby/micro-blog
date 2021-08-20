5895ab1d420ef2b5412aec15516ccd8b3b2a45b0
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-19 19:13:25 -0500

    📄 Created posts service

---
+++ posts/posts.py
@@ -0,0 +1,18 @@
+import uuid
+from flask import Blueprint, request, jsonify
+
+bp = Blueprint('posts', __name__, url_prefix=None)
+
+post_list = []
+
+@bp.route('/', methods=['GET'])
+@bp.route('/index', methods=['GET'])
+def list_posts():
+    return jsonify(post_list), 200
+
+@bp.route('/', methods=['POST'])
+def create_post():
+    req = request.get_json()
+    new_post = { 'id': str(uuid.uuid4())[:8], 'title': req['title'] }
+    post_list.append(new_post)
+    return jsonify(new_post), 201
