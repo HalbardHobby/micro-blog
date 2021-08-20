379e42af321196d1d70f0444ccb50094710641e7
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-20 14:17:33 -0500

    📖 Implemented post list

--- webclient/src/App.js
+++ webclient/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 export default () => {
   return (
@@ -12,6 +13,11 @@ export default () => {
       <div className="container">
         <h1>Create Post</h1>
         <PostCreate />
+      </div>
+      <hr />
+      <div className="container">
+        <h1>Post List</h1>
+        <PostList />
       </div>
     </div>
   );
