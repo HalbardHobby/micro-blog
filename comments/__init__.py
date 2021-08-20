aae827c5cd95fc8f800f1c99436275e27960e787
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-20 13:15:49 -0500

    ⚔ Corrected CORS issues

--- comments/__init__.py
+++ comments/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 from . import comments
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(comments.bp)
     return app
--- posts/__init__.py
+++ posts/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_cors import CORS
 
 from . import posts
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.register_blueprint(posts.bp)
     return app
