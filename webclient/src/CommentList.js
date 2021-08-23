3fbf3572194f993c18f149e6b82a0ba7f343f03f
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-23 15:41:55 -0500

    📮 frontend uses query service

--- webclient/src/CommentList.js
+++ webclient/src/CommentList.js
@@ -1,16 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export default ({ postId }) => {
-  const [comments, setComments] = useState([])
-
-  const fetchComments = async () => {
-    const res = await axios.get(`http://localhost:5001/posts/${postId}`);
-    setComments(res.data);
-  }
-
-  useEffect(() => fetchComments(), []);
-
+export default ({ comments }) => {
+  
   const renderedComments = comments.map(comment => {
     return <li key={comment.id}>{comment.content}</li>
   })
--- webclient/src/PostList.js
+++ webclient/src/PostList.js
@@ -8,13 +8,13 @@ export default () => {
   const [posts, setPosts] = useState([]);
 
   const fetchPosts = async () => {
-    const res = await axios.get("http://localhost:5000/posts/");
+    const res = await axios.get("http://localhost:5002/posts/");
     setPosts(res.data);
   };
 
   useEffect(() => fetchPosts(), []);
 
-  const renderedPosts = posts.map(post => {
+  const renderedPosts = Object.values(posts).map(post => {
     return <div 
       className="card"
       style={{ width: '30%', marginBottom: '15px' }}
@@ -22,7 +22,7 @@ export default () => {
     >
       <div className="card-body">
         <h3>{post.title}</h3>
-        <CommentList postId={post.id} />
+        <CommentList comments={post.comments} />
         <CommentCreate postId={post.id}/>
       </div>
     </div>;
