ea4d0ac69b1acbdc23a9bc691ac0d8d9edb82118
Halbard <HalbardHobby@users.noreply.github.com>
2021-08-22 20:12:18 -0500

    📮 Event handling implemented

--- query/query.py
+++ query/query.py
@@ -2,11 +2,29 @@ from flask import Blueprint, request, jsonify
 
 bp = Blueprint('query', __name__, url_prefix=None)
 
+posts = {}
+
+
 @bp.route('/events/', methods=['POST'])
 def receive_event():
-    print( 'Event received: ', jsonify(request.get_json()) )
-    return '', 200
+    req = request.get_json()
+    if req['type'] == 'PostCreated':
+        posts[req['data']['id']] = {
+            'id': req['data']['id'],
+            'title': req['data']['title'],
+            'comments': []
+        }
+    elif req['type'] == 'CommentCreated':
+        comment = {
+            'id': req['data']['id'],
+            'content': req['data']['content'],
+            'postId': req['data']['postId']
+        }
+        posts[req['data']['postId']]['comments'].append(comment)
+
+    return '', 201
+
 
 @bp.route('/posts/', methods=['GET'])
 def list_posts():
-    return '', 200
+    return jsonify(posts), 200
